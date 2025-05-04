0b4d36c002e70d49c37600e2d6ee326bdb8d7110
Fernando Ayats <ayatsfer@gmail.com>
2025-05-04 17:45:12 +0200

    Fix declarations

--- docs/options.data.js
+++ docs/options.data.js
@@ -1,17 +1,18 @@
 // @ts-check
 "use strict";
 
-import { dirname } from 'node:path'
+import { dirname, normalize } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { loadOptions, stripNixStore } from "easy-nix-documentation/loader"
+import { loadOptions } from "easy-nix-documentation/loader"
 
 export default {
     async load() {
         const __dirname = dirname(fileURLToPath(import.meta.url));
         return await loadOptions(`-f ${__dirname}/../test config.build.optionsDoc.optionsJSON`, {
             mapDeclarations: declaration => {
-                const relDecl = stripNixStore(declaration);
-                return `<a href="http://github.com/viperML/nix-maid/tree/master/${relDecl}">&lt;${relDecl}&gt;</a>`
+                const root = normalize(`${__dirname}/..`);
+                const relDecl = declaration.replace(root, "");
+                return `<a href="http://github.com/viperML/nix-maid/tree/master${relDecl}">&lt;nix-maid${relDecl}&gt;</a>`
             },
         })
     }
