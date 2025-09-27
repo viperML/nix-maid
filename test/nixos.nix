cf8c5940829a7fb44ad0e6e9acc9427ab1db588d
Fernando Ayats <ayatsfer@gmail.com>
2025-09-27 12:35:18 +0200

    Tweak test layout

--- test/nixos.nix
+++ test/nixos.nix
@@ -1,42 +1,39 @@
 let
   pkgs = import <nixpkgs> { };
+in
+pkgs.nixos {
+  imports = [
+    (import ../.).nixosModules.default
+    (pkgs.path + /nixos/modules/virtualisation/qemu-vm.nix)
+  ];
 
-  nixos = pkgs.nixos {
-    imports = [
-      (import ../.).nixosModules.default
-      (pkgs.path + /nixos/modules/virtualisation/qemu-vm.nix)
-    ];
-
-    users.users.nixos = {
-      isNormalUser = true;
-      maid = {
-        imports = [
-          {
-            file.home.bar.source = "/dev/null";
-          }
-        ];
-        file.home."foo".source = "/dev/null";
-        systemd.services."test" = {
-          script = ''
-            pwd
-          '';
-          serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = true;
-          };
-          wantedBy = [ "default.target" ];
-
+  users.users.nixos = {
+    isNormalUser = true;
+    maid = {
+      imports = [
+        {
+          file.home.bar.source = "/dev/null";
+        }
+      ];
+      file.home."foo".source = "/dev/null";
+      systemd.services."test" = {
+        script = ''
+          pwd
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
         };
+        wantedBy = [ "default.target" ];
+
       };
-      extraGroups = [ "wheel" ];
     };
-
-    virtualisation = {
-      graphics = false;
-    };
-
-    services.getty.autologinUser = "nixos";
+    extraGroups = [ "wheel" ];
   };
 
-in
-nixos.config.system.build.vm
+  virtualisation = {
+    graphics = false;
+  };
+
+  services.getty.autologinUser = "nixos";
+}
