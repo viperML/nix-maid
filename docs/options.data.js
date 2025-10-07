c1025492a52a5081da99a3a21df7b8c845d15363
Fernando Ayats <fayats@bsc.es>
2025-10-07 15:59:43 +0200

    Fix doc generation

--- docs/options.data.js
+++ docs/options.data.js
@@ -8,7 +8,7 @@ import { loadOptions } from "easy-nix-documentation/loader"
 export default {
     async load() {
         const __dirname = dirname(fileURLToPath(import.meta.url));
-        return await loadOptions(`-f ${__dirname}/../test config.build.optionsDoc.optionsJSON`, {
+        return await loadOptions(`-f ${__dirname}/../test/simple.nix config.build.optionsDoc.optionsJSON`, {
             mapDeclarations: declaration => {
                 const root = normalize(`${__dirname}/..`);
                 const relDecl = declaration.replace(root, "");
